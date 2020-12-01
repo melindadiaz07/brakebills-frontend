5744b795281f71bfd297b75861a857a4d2f1e9c1
Melindadiaz07 <melindadiaz@me.com>
2020-12-01 11:44:06 -0500

    Fixed private and public carousel bug

--- src/components/PublicCarousel.js
+++ src/components/PublicCarousel.js
@@ -37,17 +37,14 @@ const PublicCarousel = () => {
 
    
 </div>
-    <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-
-  
-
   
     </div>
 
