0548c658e619eb572d359b202aa7c8af59361e40
Melindadiaz07 <melindadiaz@me.com>
2020-12-01 11:41:07 -0500

    Added text carousel components to create an annoucements ticker on home page

---
+++ src/components/AnnouncementsCarousel.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export const fake = () => {
+   return(
+    <div>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      Brakebills campus will be closed until March 6th for repair of a gas leak
+    </div>
+    <div class="carousel-item">
+    Brakebills campus will be closed until December 6th for repair of a gas leak
+
+    </div>
+    <div class="carousel-item">
+    Brakebills campus will be closed until March 6th for repair of a gas leak
+
+    </div>
+  </div>
+</div>
+
+    </div>
+   )
+}
+
+
+
+
+export const real= () => {
+  return(
+    
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      Brakebills campus will be closed until March 6th due to a lycanthropy outbreak
+    </div>
+    <div class="carousel-item">
+    Brakebills campus will be closed until December 6th for repair of a gas leak
+
+    </div>
+    <div class="carousel-item">
+    Brakebills campus will be closed until March 6th for repair of a gas leak
+
+    </div>
+  </div>
+</div>
+  )
+}
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import video from '../images/classroomshot.mp4'
+import * as announcements from './AnnouncementsCarousel'
 
 
 
 const Home = ({logged_in}) => {
 
+// const [fakeAnnouncement, setFakeAnnouncement] = useState("")
+// const [realAnnouncement, setRealAnnouncement] = useState("")
 
+
+// const announcements = () => {
+//   setInterval(() => {
+   
+//   }, time)
+// }
 
 return(
   <div className="home-page-container">
@@ -16,7 +25,7 @@ return(
           <div className="home-page-header">
           <video autoPlay loop muted src={video} type="video/mp4" width="100%" />
             <h2>Brakebills <br></br><span className="university-span">University</span> <span className="header-span">of Arts and Sciences</span></h2></div>
-            <div className="announcements-container"><h3>Announcements:  Brakebills campus will be closed until March 3rd for repair of a gas leak.</h3></div>
+        <div className="announcements-container"><h3> { announcements.fake() } </h3></div>
             
         </div>
 
@@ -28,7 +37,7 @@ return(
           <div className="home-page-header">
           <video autoPlay loop muted src={video} type="video/mp4" width="100%"/>
             <h2>Brakebills <br></br><span className="university-span">University</span><br></br><span className="header-span">for Magical Pedogogy</span></h2></div>
-            <div className="announcements-container"><h3>Announcements:  Brakebills campus will be closed until March 3rd for repair of a gas leak.</h3></div>
+            <div className="announcements-container"><h3>{ announcements.real() }</h3></div>
             
         </div>
 
